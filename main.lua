10e2e5db1cb97e1881e5881362703376ab870cae
bellia_n <belliardnathan@gmail.com>
2017-01-21 18:29:43 +0100

    test player object

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+require "src/player"
+
+function love.load()
+  love.graphics.setBackgroundColor(255, 2555, 255)
+  --Loading player--
+  player.load()
+end
+
+function love.update(dt)
+  player.update(dt)
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+  player.draw()
 end
---
+++ src/player.lua
@@ -0,0 +1,21 @@
+player = {}
+
+function player.load()
+  player.x = 5
+  player.y = 5
+  player.xvel = 0
+  player.yvel = 0
+  player.friction = 7
+  player.speed = 2250
+  player.width = 50
+  player.height= 50
+end
+
+function player.draw()
+  love.graphics.setColor(255, 0, 0)
+  love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+end
+
+function player.update(dt)
+
+end
