cf7d68b59e124c99c20b8984871c7c17b01eef0e
querat_g <querat_g@epitech.eu>
2017-01-21 18:21:17 +0100

    science

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+--[[
+   Doing some science around here   
+--]]
+
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
+end
+
+function love.update()
+   print(love.mouse.getPosition())
+end
+
+function love.mousepressed(x, y, button, istouch)
+   print("x:", x, " y:", y, " button:", button, " istouch:", istouch)
 end
